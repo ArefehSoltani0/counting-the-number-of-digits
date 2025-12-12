27879b6e5602ece627e81c0773f6313ed93a6b11
PARSA <PARSA@DESKTOP-UNKQQTA>
2025-12-12 19:24:04 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        int d =int.Parse(Console.ReadLine());
+        Console.WriteLine(count(d));
+        Console.ReadKey();
+    }
+    static int count(int n)
+    {
+        if (n < 10)
+            return 1;
+        else
+            return 1 + count(n / 10);
+    }
+}
